cec5cce8ca4edfb37231b7bd71ca5420420c085f
yam24bot <danil180401@gmail.com>
2019-01-22 16:15:10 +0200

    Add def edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,41 @@
 class UsersController < ApplicationController
 
+  before_action :authenticate_user!, only: [:edit, :update]
+  before_action :check_autorization, only: [:edit, :update]
+  before_action :set_user
+
   def show
     @user = User.find(params[:id])
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      flash.now[:alert] = "somethig wrong"
+      render :edit
+    end
+  end
+
+  private
+
+  def check_autorization
+    unless current_user.id == params[:id].to_i
+      redirect_to root_url
+    end
+
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+      params.require(:user).permit(:first_name)
   end
 
 end
