9f9d20db95afd5535a63be3b91bef5850593c1e3
yam24bot <danil180401@gmail.com>
2019-01-23 15:53:30 +0200

    Add ban

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
 
   resources :videos
   resources :users
+  resources :banned
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
