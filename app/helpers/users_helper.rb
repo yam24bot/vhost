7a47ac0a585a01f2cc9dfb2ac169c9a69423b850
yam24bot <danil180401@gmail.com>
2019-01-29 11:55:03 +0200

    Add preview on users edit page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,10 @@
 module UsersHelper
-  def avatar_for(user, option = {size: 80})
+  def avatar_for(user, option = {size: 100})
     size = option[:size]
     if user.avatar?
-      image_tag user.avatar.url(:thumb), width: size, class: 'avatar-image'
+      image_tag user.avatar.url(:thumb), width: size, height:size, class: 'avatar-image'
     else
-      image_tag "defaut_avatar.png", width: size, class: 'avatar-image'
+      image_tag "defaut_avatar.png", width: size, height:size, class: 'avatar-image'
     end
   end
 
